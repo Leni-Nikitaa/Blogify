f7b5bb013f8236122918e76ea23443e1df588a80
Leni-Nikitaa <nickytaaleni@gmail.com>
2024-03-08 14:39:16 +0530

    Updated Injex ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 router.get("", (req, res) => {
-  res.send("Hello World!");
+  const locals = {
+    title: "Blogify",
+    description:
+      "A dynamic blog platform for avid readers and aspiring writers, built with HTML, CSS, Node.js, and MongoDB. Seamlessly store and retrieve your posts, interact with a vibrant community, and dive into a world of literary exploration.",
+  };
+  res.render("index", { locals });
+});
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 module.exports = router;
