5e702e740f039fa333227971cef2e082799eff1c
Leni-Nikitaa <nickytaaleni@gmail.com>
2024-03-16 22:52:14 +0530

    Updates Script js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const connectDB = require("./server/config/db");
 const app = express();
 const PORT = 5000 || process.env.PORT;
 connectDB();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 app.use(expressLayout);
 app.set("layout", "./layouts/main");
--- server/routes/main.js
+++ server/routes/main.js
@@ -86,6 +86,43 @@ router.get("", async (req, res) => {
 }
 
 insertPostData();*/
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
+    const locals = {
+      title: data.title,
+      description:
+        "A dynamic blog platform for avid readers and aspiring writers, built with HTML, CSS, Node.js, and MongoDB. Seamlessly store and retrieve your posts, interact with a vibrant community, and dive into a world of literary exploration.",
+    };
+    res.render("post", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.post("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description:
+        "A dynamic blog platform for avid readers and aspiring writers, built with HTML, CSS, Node.js, and MongoDB. Seamlessly store and retrieve your posts, interact with a vibrant community, and dive into a world of literary exploration.",
+    };
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    });
+    res.render("search", {
+      data,
+      locals,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 router.get("/about", (req, res) => {
   res.render("about");
 });
